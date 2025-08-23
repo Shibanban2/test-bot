8a389de7a95cb8eca09b36f915f7fbd880f656b3
Shibanban2 <salenyanko12891745@gmail.com>
2025-08-23 11:41:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import os
-from os.path import dirname, join
-
 import discord
+import aiohttp
+import asyncio
 from dotenv import load_dotenv
 
 from keep_alive import keep_alive
 
 intents = discord.Intents.default()
-intents.messages = True   # メッセージ受信のIntentを有効化
-intents.message_content = True  # メッセージ内容を受け取れるようにする
+intents.messages = True
+intents.message_content = True
 client = discord.Client(intents=intents)
 
 load_dotenv(verbose=True)
+
+async def fetch_tsv(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            text = await resp.text()
+            rows = [line.split("\t") for line in text.strip().split("\n")]
+            return rows
 
 @client.event
 async def on_ready():
@@ -19,13 +26,27 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    # Bot自身のメッセージは無視
     if message.author == client.user:
         return
 
-    # メッセージが "event" の場合に返信
     if message.content.lower() == "event":
         await message.channel.send("イベントだよ！")
+
+    if message.content.startswith("sale "):
+        _, sale_id = message.content.split(" ", 1)
+        url = "https://shibanban2.github.io/bc-event/token/sale.tsv"
+        rows = await fetch_tsv(url)
+
+        found = None
+        for row in rows:
+            if sale_id in row:  # 行にそのIDが含まれていれば
+                found = "\t".join(row)
+                break
+
+        if found:
+            await message.channel.send(found)
+        else:
+            await message.channel.send(f"ID {sale_id} は見つかりませんでした")
 
 keep_alive()
 
@@ -34,4 +55,5 @@ if TOKEN:
     client.run(TOKEN)
 else:
     print("Tokenが見つかりませんでした")
+
 
