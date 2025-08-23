c54146f0fb4f033ee462a1fcd35c83aa8382b809
Shibanban2 <salenyanko12891745@gmail.com>
2025-08-23 11:25:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from dotenv import load_dotenv
 
 from keep_alive import keep_alive
 
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.default()
+intents.messages = True   # メッセージ受信のIntentを有効化
+intents.message_content = True  # メッセージ内容を受け取れるようにする
+client = discord.Client(intents=intents)
 
 load_dotenv(verbose=True)
 
@@ -14,6 +17,15 @@ load_dotenv(verbose=True)
 async def on_ready():
     print("ログインしました")
 
+@client.event
+async def on_message(message):
+    # Bot自身のメッセージは無視
+    if message.author == client.user:
+        return
+
+    # メッセージが "event" の場合に返信
+    if message.content.lower() == "event":
+        await message.channel.send("イベントだよ！")
 
 keep_alive()
 
@@ -22,3 +34,4 @@ if TOKEN:
     client.run(TOKEN)
 else:
     print("Tokenが見つかりませんでした")
+
